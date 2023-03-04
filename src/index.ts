16d9f0c2796be418c30334f3fb7589c4d900d36a
olaf <gwanyong.dev@gmail.com>
2023-03-04 17:50:47 +0900

    feature: 기능 구현코드

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import App from './App';
-document.getElementById('app')!.appendChild(App());
+declare interface ElementEvent extends Event {
+  currentTarget: HTMLElement;
+  target: HTMLElement;
+}
+
+function init() {}
+
+document.addEventListener('DOMContentLoaded', init);
